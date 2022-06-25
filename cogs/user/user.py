4e09663729a2f9ecd13b00815fb9d2e4524d9073
yViini <83708990+luisviniciuslv@users.noreply.github.com>
2022-06-25 18:35:28 -0300

    edit

--- cogs/user/user.py
+++ cogs/user/user.py
@@ -43,12 +43,7 @@ class User(commands.Cog):
             #day
             if int(str(Last_hour).split(' ')[0].split('-')[2]) + int(str(current_time).split(' ')[0].split('-')[2]) >= 2:
                 return 'max'
-            if int(str(Last_hour).split(' ')[0].split('-')[2]) > int(str(current_time).split(' ')[0].split('-')[2]):
-                if str(Last_hour).split(' ')[1].split(':')[0] < str(current_time).split(' ')[1].split(':')[0]:
-                    horas_trabalhadas = (int(str(current_time).split(' ')[1].split(':')[1]) + 24) - int(str(Last_hour).split(' ')[1].split(':')[1])
-                    return round(horas_trabalhadas * money_hour/24)
-                else:
-                    return 'max'
+        
             #hour negative
             if str(Last_hour).split(' ')[1].split(':')[0] < str(current_time).split(' ')[1].split(':')[0]:
                 minutos_trabalhados = (int(str(current_time).split(' ')[1].split(':')[1]) + 60) - int(str(Last_hour).split(' ')[1].split(':')[1])
